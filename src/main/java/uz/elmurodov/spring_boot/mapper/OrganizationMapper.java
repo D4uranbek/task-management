c65e9d5275c4904739dfe7d5ece7bfc0ab56693b
D4uranbek <d4uranbek@gmail.com>
2022-02-22 11:11:19 +0500

    initial

---
+++ src/main/java/uz/elmurodov/spring_boot/mapper/OrganizationMapper.java
@@ -0,0 +1,30 @@
+package uz.elmurodov.spring_boot.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.springframework.stereotype.Component;
+import uz.elmurodov.spring_boot.dto.organization.OrganizationCreateDto;
+import uz.elmurodov.spring_boot.dto.organization.OrganizationDto;
+import uz.elmurodov.spring_boot.dto.organization.OrganizationUpdateDto;
+import uz.elmurodov.spring_boot.entity.organization.Organization;
+
+@Component
+@Mapper(componentModel = "spring")
+public interface OrganizationMapper extends BaseMapper<
+        Organization,
+        OrganizationDto,
+        OrganizationCreateDto,
+        OrganizationUpdateDto> {
+
+    @Override
+    @Mapping(target = "logo", ignore = true)
+    Organization fromCreateDto(OrganizationCreateDto organizationCreateDto);
+
+    @Override
+    @Mapping(target = "logo", ignore = true)
+    Organization fromUpdateDto(OrganizationUpdateDto organizationUpdateDto);
+
+    @Mapping(target = "logo", ignore = true)
+    Organization fromUpdateDto(OrganizationUpdateDto organizationUpdateDto, @MappingTarget Organization organization);
+}
